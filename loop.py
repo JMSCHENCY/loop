083b9a1982dcfbdd7102936f56c1dbc046b8eacc
James Chen <jmschency@gmail.com>
2018-12-20 17:33:42 +0800

    Add new file

---
+++ loop.py
@@ -0,0 +1,25 @@
+# for loop
+# cars
+cars = ['Toyota', 'Honda']
+
+for car in cars:
+    print(car)
+
+# names
+names = ['Allen', 'Tom', 'Mayday', 'JJ', 'Jone', 'Jay', 'James']
+
+for name in names:
+    print('Hi,', name, '!')
+
+
+# 字串當清單
+car = 'Audi'
+
+for c in car:
+    print(c)
+
+print(len(car))
+print('A' in car)
+print('d' in car)
+print('Au' in car)
+print('x' in car)
